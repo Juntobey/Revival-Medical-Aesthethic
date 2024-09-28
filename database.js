8e8297900c336fd5ef9db40b0b40675d67eb511d
Tobey Ndlovu <117341626+Juntobey@users.noreply.github.com>
2024-09-28 09:12:00 +0200

    Create database.js

---
+++ database.js
@@ -0,0 +1,18 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+const pool = new Pool({
+    user:  process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.DB_PORT,
+    // Default port
+});
+
+console.log(pool)
+
+module.exports = pool;
